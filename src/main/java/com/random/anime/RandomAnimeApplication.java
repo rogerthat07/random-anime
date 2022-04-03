6a94868d63131ca72ca83e3b9fbba6c31511c9be
unknown <singh.swaraj1999@gmail.com>
2022-04-03 13:10:32 +0530

    initial project setup

---
+++ src/main/java/com/random/anime/RandomAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.random.anime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomAnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/random/anime/RandomAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.random.anime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
