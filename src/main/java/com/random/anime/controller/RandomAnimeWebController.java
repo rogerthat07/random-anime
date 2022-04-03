d50725f87d0658ea1e6e012a24d1c85b472fff31
unknown <singh.swaraj1999@gmail.com>
2022-04-03 15:14:11 +0530

    used exchange method of resttemplate to add headers to successfully fetch result

--- src/main/java/com/random/anime/controller/RandomAnimeWebController.java
+++ src/main/java/com/random/anime/controller/RandomAnimeWebController.java
@@ -1,14 +1,23 @@
 package com.random.anime.controller;
 
+import com.random.anime.model.ResponsePojo;
+import com.random.anime.tools.api.KitsuApi;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RandomAnimeWebController {
+
+    @Autowired
+    private KitsuApi api;
     
     @GetMapping("/")
     public ModelAndView home(){
+        ResponsePojo responsePojo = api.fetchAnime();
+        System.out.println(responsePojo);
         return new ModelAndView("home");
     }
 }
--- src/main/java/com/random/anime/tools/api/KitsuApi.java
+++ src/main/java/com/random/anime/tools/api/KitsuApi.java
@@ -1,6 +1,11 @@
 package com.random.anime.tools.api;
 
+import com.random.anime.model.ResponsePojo;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,4 +15,13 @@ public class KitsuApi {
     @Autowired
     private RestTemplate restTemplate;
     
+    public ResponsePojo fetchAnime(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json, application/vnd.api+json");
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+        //ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class, param);
+        String url = "https://kitsu.io/api/edge/anime?sort=popularityRank&page[limit]=1";
+        ResponsePojo response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, ResponsePojo.class).getBody();
+        return response;
+    }
 }
